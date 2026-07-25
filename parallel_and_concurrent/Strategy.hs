43856cafee2dfa4272d870570e7c0ffc50ae0d7d
theo <theotirel@gmail.com>
2026-07-25 18:21:16 +0800

    [parallel] Strategy fix

--- parallel_and_concurrent/Strategy.hs
+++ parallel_and_concurrent/Strategy.hs
@@ -1,14 +1,18 @@
 module Main where
 
 import Control.Parallel.Strategies
+import Control.DeepSeq (force)
 
-type Strategy a = a -> Eval a
+main :: IO ()
+main = pure ()
+
+-- type Strategy a = a -> Eval a
 
 parPair :: Strategy (a,b)
 parPair (a,b) = do
-	 a' <- rpar a 
-	 b' <- rpar b
-	 return (a',b')
+  a' <- rpar a 
+  b' <- rpar b
+  return (a',b')
 
 using :: a -> Strategy a -> a
 x `using` s = runEval (s x)
@@ -17,17 +21,20 @@ x `using` s = runEval (s x)
 
 evalPair :: Strategy a -> Strategy b -> Strategy (a,b)
 evalPair sa sb (a,b) = do
-	 a' <- sa a
-	 b' <- sb b
-	 return (a',b')
+  a' <- sa a
+  b' <- sb b
+  return (a',b')
 
-parPair :: Strategy (a,b)
-parPair = evalPair rpar rpar
+parPair' :: Strategy (a,b)
+parPair' = evalPair rpar rpar
 
 rdeepseq :: NFData a => Strategy a
 rdeepseq x = rseq (force x)
 
-rparWith :: Strategy a -> Strategy a -- rpaWith s = wrap the Stregy s in an rpar
-rparWith s = s >== \x -> rpar x
+r0 :: Strategy a
+r0 x = return x
 
-
+rparWith :: Strategy a -> Strategy a -- rparWith s = wrap the Stregy s in an rpar
+rparWith s = \x -> do
+  rpar $ s x
+  return x
