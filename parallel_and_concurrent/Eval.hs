332efae5d67e0330c3a4c352a62235d782f60754
theo <theotirel@gmail.com>
2026-07-25 18:33:56 +0800

    [parallel] fix Eval

--- parallel_and_concurrent/Eval.hs
+++ parallel_and_concurrent/Eval.hs
@@ -2,11 +2,34 @@ module Main where
 
 import Control.Parallel.Strategies
 
-data Eval a
-instance Monad Eval
+{-
+newtype Eval a = Done a
+
 runEval :: Eval a -> a
+runEval (Done x) = x
+
+instance Functor Eval where
+  fmap f (Done x) = Done (f x)
+
+instance Applicative Eval where
+  pure x = Done x
+  Done f <*> Done x = Done (f x)
+
+instance Monad Eval where
+  return x = Done x
+  Done x >>= k = k x   -- Pattern matching on 'Done x' forces sequencing
+
+
+-- | Spark the argument in parallel to be evaluated to WHNF
 rpar :: a -> Eval a
+rpar x = x `par` Done x
+
+-- | Evaluate the argument sequentially to WHNF right now
 rseq :: a -> Eval a
+rseq x = x `pseq` Done x 
+
+pseq forbids the compiler to reorder 
+-}
 
 f :: Num a => a -> a
 f x = 2
@@ -18,20 +41,20 @@ y :: Int
 y = 2
 
 g = runEval $ do
-	 a <- rpar (f x)
-	 b <- rpar (f y)
-	 -- returns straight away the evalution continue in parallel 
-	 return (a, b)
+  a <- rpar (f x)
+  b <- rpar (f y)
+  -- returns straight away the evalution continue in parallel 
+  return (a, b)
 
 l = runEval $ do
-	 a <- rpar (f x)
-	 b <- rseq (f y)
-	 -- returns after b evaluates
-	 return (a, b)
+  a <- rpar (f x)
+  b <- rseq (f y)
+  -- returns after b evaluates
+  return (a, b)
 main :: IO ()
 main = do 
-	 let (a,b) = g
-	 putStrLn $ "a = " ++ show a ++ ", b = " ++ show b
+  let (a,b) = g
+  putStrLn $ "a = " ++ show a ++ ", b = " ++ show b
 
 
 
