460b22a920d5d34921c44fc053c9225709a474e1
theo <theotirel@gmail.com>
2026-07-25 18:36:48 +0800

    [parellel] fixing NFData

--- parallel_and_concurrent/NFData.hs
+++ parallel_and_concurrent/NFData.hs
@@ -1,13 +1,19 @@
-class NFData a where -- reduce normal form
-rnf :: a -> ()
+module Main where
+
+main :: IO ()
+main = pure ()
+
+class NFData a where
+  rnf :: a -> ()
 
 data Tree a = Empty | Branch (Tree a) a (Tree a)
+
 instance NFData a => NFData (Tree a) where
-rnf Empty = ()
-rnf (Branch l a r) = rnf l `seq ` rnf a `seq` rnf r
+  rnf Empty = ()
+  rnf (Branch l a r) = rnf l `seq` rnf a `seq` rnf r
 
 deepseq :: NFData a => a -> b -> b
 deepseq a b = rnf a `seq` b
 
 force :: NFData a => a -> a
-force x = x `deepseq`x
+force x = x `deepseq` x
