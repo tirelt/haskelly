951837c3f8b61f6eaef930ae0b31efce27231dba
theo <theotirel@gmail.com>
2026-08-02 13:23:42 +0800

    [parallel] exceptions in async

--- parallel_and_concurrent/Async.hs
+++ parallel_and_concurrent/Async.hs
@@ -1,6 +1,7 @@
 module Main where 
 
 import Control.Concurrent
+import  Control.Exception
 
 main :: IO ()
 main = pure ()
@@ -15,3 +16,37 @@ async action = do
 
 wait :: Async a -> IO a
 wait (Async var) = readMVar var
+
+-- Version with exceptions handling
+
+data Async' a = Async' (MVar (Either SomeException a))
+
+async' :: IO a -> IO (Async' a)
+async' action = do
+  var <- newEmptyMVar
+  forkIO (do r <- try action; putMVar var r)
+  return (Async' var)
+
+waitCatch :: Async' a -> IO (Either SomeException a)
+waitCatch (Async' var) = readMVar var
+
+wait' :: Async' a -> IO a
+wait' a = do
+  r <- waitCatch a
+  case r of
+    Left e -> throwIO e
+    Right a -> return a
+
+waitEither :: Async' a -> Async' b -> IO (Either a b)
+waitEither a b = do
+  m <- newEmptyMVar
+  forkIO $ do r <- try (fmap Left (wait' a)); putMVar m r
+  forkIO $ do r <- try (fmap Right (wait' b)); putMVar m r
+  wait' (Async' m)
+
+waitAny :: [Async' a] -> IO a -- simple but frutrating to create new thread for each async operation
+waitAny as = do
+  m <- newEmptyMVar
+  let forkwait a = forkIO $ do r <- try (wait' a); putMVar m r
+  mapM_ forkwait as
+  wait' (Async' m)
